f53f5f7d1f400ef99112177e4ccf76e13bb788e5
Ainsley Clark <info@ainsleyclark.com>
2023-01-29 09:19:46 +0000

    Map, styling & scripting helpers

---
+++ assets/js/components/map.ts
@@ -0,0 +1,214 @@
+/**
+ * map.ts
+ *
+ * @author Ainsley Clark
+ * @author URL:   https://ainsley.dev
+ * @author Email: hello@ainsley.dev
+ * @see https://developers.google.com/maps/documentation/javascript/overview
+ */
+
+import {Loader} from "@googlemaps/js-api-loader"
+
+/**
+ * Load the Google map with the API key.
+ */
+const loader = new Loader({
+	apiKey: "AIzaSyChJnTW-pCWmAQ3Jzzh3-xfve4JOsuMqMo",
+	version: "weekly",
+});
+
+/**
+ * Once the loader has finished, initialise map variable and
+ * set the zoom, lat and long and the custom marker.
+ */
+const load = () => {
+	loader.load().then(() => {
+		const position = {
+			lat: 51.5155705754434,
+			lng: -0.12367415060938884,
+		} as google.maps.LatLngLiteral;
+
+		const map = new google.maps.Map(document.querySelector(".map") as HTMLElement, {
+			backgroundColor: '#ffffff',
+			center: position,
+			zoom: 16,
+			styles: style,
+			streetViewControl: false,
+			mapTypeControl: false,
+		});
+
+		new google.maps.Marker({
+			position: position,
+			map: map,
+			icon: "/images/icons/marker.svg"
+		});
+	});
+}
+
+export default load;
+
+/**
+ * Styling for the map as a JSON array.
+ * @see https://mapstyle.withgoogle.com/
+ */
+const style = [
+	{
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#f5f5f5"
+			}
+		]
+	},
+	{
+		"elementType": "labels.icon",
+		"stylers": [
+			{
+				"color": "#f5f5f5",
+			}
+		]
+	},
+	{
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#616161"
+			}
+		]
+	},
+	{
+		"elementType": "labels.text.stroke",
+		"stylers": [
+			{
+				"color": "#f5f5f5"
+			}
+		]
+	},
+	{
+		"featureType": "administrative.land_parcel",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#bdbdbd"
+			}
+		]
+	},
+	{
+		"featureType": "poi",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#eeeeee"
+			}
+		]
+	},
+	{
+		"featureType": "poi",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#757575"
+			}
+		]
+	},
+	{
+		"featureType": "poi.park",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#e5e5e5"
+			}
+		]
+	},
+	{
+		"featureType": "poi.park",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+		]
+	},
+	{
+		"featureType": "road",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#ffffff"
+			}
+		]
+	},
+	{
+		"featureType": "road.arterial",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#757575"
+			}
+		]
+	},
+	{
+		"featureType": "road.highway",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#dadada"
+			}
+		]
+	},
+	{
+		"featureType": "road.highway",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#616161"
+			}
+		]
+	},
+	{
+		"featureType": "road.local",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+		]
+	},
+	{
+		"featureType": "transit.line",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#e5e5e5"
+			}
+		]
+	},
+	{
+		"featureType": "transit.station",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#eeeeee"
+			}
+		]
+	},
+	{
+		"featureType": "water",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"color": "#c9c9c9"
+			}
+		]
+	},
+	{
+		"featureType": "water",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+		]
+	}
+]
+
---
+++ assets/js/pages/contact.ts
@@ -0,0 +1,11 @@
+/**
+ * contact.ts
+ *
+ * @author Ainsley Clark
+ * @author URL:   https://ainsley.dev
+ * @author Email: hello@ainsley.dev
+ */
+
+import load from "../components/map";
+
+load();
