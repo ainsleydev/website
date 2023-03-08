a405bced7e76a4a77b804d65981a9a7a6654cad3
Ainsley Clark <hello@ainsley.dev>
2023-03-08 09:38:52 +0000

    feat: Working on text animation

--- assets/js/animations/text.ts
+++ assets/js/animations/text.ts
@@ -9,7 +9,7 @@
 import anime from 'animejs/lib/anime.es';
 
 const textWrapper = document.querySelector('h1');
-if (textWrapper && textWrapper.textContent) {
+if (textWrapper) {
 	textWrapper.innerHTML = textWrapper.innerHTML.replace(/(?![^<]*>)[^<]/g, (c) => {
 		if (c == ' ') {
 			return c;
@@ -19,71 +19,57 @@ if (textWrapper && textWrapper.textContent) {
 		}
 		return `<span class="letter">${c}</span>`;
 	});
-	textWrapper.querySelector('u').innerHTML += `<span class="underline">`;
-}
+	const underline = textWrapper.querySelector('u');
+	if (underline) {
+		underline.innerHTML += `<span class="underline">`;
+	}
 
-anime.set('h1', { opacity: 1 });
+	anime.set('h1', { opacity: 1 });
 
-// const test = anime.timeline()
-// 	.add({
-// 		targets: 'h1 .letter',
-// 		translateY: [100,0],
-// 		translateZ: 0,
-// 		opacity: [0,1],
-// 		easing: "easeOutExpo",
-// 		duration: 1400,
-// 		delay: (el, i) => 300 + 50 * i,
-// 	})
-// 	.add({
-// 		targets: 'h1 .mark',
-// 		scale: [3.6, 1],
-// 		opacity: [0, 1],
-// 		rotateZ: [45, 0],
-// 		easing: "easeInOutExpo",
-// 		duration: 1000,
-// 	})
-// 	.add({
-// 		targets: ".underline",
-// 		opacity: [0.5,1],
-// 		scaleX: [0, 1],
-// 		easing: "easeInOutExpo",
-// 		duration: 700
-// 	}, '-=1200')
-// 	.add({
-// 		targets: ".lead",
-// 		translateY: [100,0],
-// 		translateZ: 0,
-// 		easing: "easeOutExpo",
-// 		opacity: [0,1],
-// 	}, "-=1000")
+	const els = textWrapper.querySelectorAll('.mark, .letter, .underline');
+	const markIndex = [...els].indexOf(textWrapper.querySelector('.mark')),
+		lineIndex = [...els].indexOf(textWrapper.querySelector('.underline'));
 
-const timeline = anime.timeline({
-	autoplay: false,
-});
-textWrapper.querySelectorAll('.letter, .mark').forEach((child, index) => {
-	const delay = index * 50;
-	if (child.classList.contains('letter')) {
-		timeline.add(
-			{
-				targets: child,
-				translateY: [100, 0],
-				translateZ: 0,
-				opacity: [0, 1],
-				easing: 'easeOutExpo',
-			},
-			delay,
-		);
-	} else if (child.classList.contains('mark')) {
-		timeline.add(
+	const test = anime
+		.timeline()
+		.add({
+			targets: 'h1 .letter',
+			translateY: [100, 0],
+			translateZ: 0,
+			opacity: [0, 1],
+			easing: 'easeOutExpo',
+			duration: 1400,
+			delay: (el, i) => 300 + 50 * i,
+		})
+		.add(
 			{
 				targets: 'h1 .mark',
 				scale: [3.6, 1],
 				opacity: [0, 1],
 				rotateZ: [45, 0],
 				easing: 'easeInOutExpo',
+				duration: 1000,
 			},
-			delay,
+			300 + 50 * markIndex,
+		)
+		.add(
+			{
+				targets: '.underline',
+				opacity: [0.5, 1],
+				scaleX: [0, 1],
+				easing: 'easeInOutExpo',
+				duration: 700,
+			},
+			300 + 100 * lineIndex,
+		)
+		.add(
+			{
+				targets: '.lead',
+				translateY: [100, 0],
+				translateZ: 0,
+				easing: 'easeOutExpo',
+				opacity: [0, 1],
+			},
+			'-=1000',
 		);
-	}
-});
-timeline.play();
+}
