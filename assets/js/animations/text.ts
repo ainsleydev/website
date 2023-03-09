2749b3cca87ccd8a38b20666d426257f5ffb63fa
Ainsley Clark <hello@ainsley.dev>
2023-03-09 07:32:35 +0000

    feat: Working on text animations

--- assets/js/animations/text.ts
+++ assets/js/animations/text.ts
@@ -8,32 +8,34 @@
 
 import anime from 'animejs/lib/anime.es';
 
-const textWrapper = document.querySelector('h1');
+const textWrapper = document.querySelector('.text-animate');
 if (textWrapper) {
+	// TODO, strings.Trim whitespace around text.
 	textWrapper.innerHTML = textWrapper.innerHTML.replace(/(?![^<]*>)[^<]/g, (c) => {
 		if (c == ' ') {
 			return c;
 		}
 		if (c == '*') {
-			return `<span class="mark">${c}</span>`;
+			return `<span class="text-animate-mark">${c}</span>`;
 		}
-		return `<span class="letter">${c}</span>`;
+		return `<span class="text-animate-letter">${c}</span>`;
 	});
 	const underline = textWrapper.querySelector('u');
 	if (underline) {
-		underline.innerHTML += `<span class="underline">`;
+		underline.innerHTML += `<span class="text-animate-underline">`;
 	}
 
 	anime.set('h1', { opacity: 1 });
 
-	const els = textWrapper.querySelectorAll('.mark, .letter, .underline');
-	const markIndex = [...els].indexOf(textWrapper.querySelector('.mark')),
-		lineIndex = [...els].indexOf(textWrapper.querySelector('.underline'));
+	const els = textWrapper.querySelectorAll('.text-animate-mark, .text-animate-letter, .text-animate-underline');
+	const markIndex = [...els].indexOf(textWrapper.querySelector('.text-animate-mark')),
+		lineIndex = [...els].indexOf(textWrapper.querySelector('.text-animate-underline'));
 
-	const test = anime
+	// TODO, we can pass in the elements instead of the text string.
+	anime
 		.timeline()
 		.add({
-			targets: 'h1 .letter',
+			targets: '.text-animate-letter',
 			translateY: [100, 0],
 			translateZ: 0,
 			opacity: [0, 1],
@@ -43,7 +45,7 @@ if (textWrapper) {
 		})
 		.add(
 			{
-				targets: 'h1 .mark',
+				targets: '.text-animate-mark',
 				scale: [3.6, 1],
 				opacity: [0, 1],
 				rotateZ: [45, 0],
@@ -54,22 +56,23 @@ if (textWrapper) {
 		)
 		.add(
 			{
-				targets: '.underline',
+				targets: '.text-animate-underline',
 				opacity: [0.5, 1],
 				scaleX: [0, 1],
 				easing: 'easeInOutExpo',
-				duration: 700,
+				duration: 1000,
 			},
-			300 + 100 * lineIndex,
+			250 + 50 * lineIndex,
 		)
 		.add(
 			{
-				targets: '.lead',
-				translateY: [100, 0],
+				targets: '.text-animate-lead',
+				translateY: [50, 0],
 				translateZ: 0,
 				easing: 'easeOutExpo',
 				opacity: [0, 1],
+				duration: 1500,
 			},
-			'-=1000',
+			'-=900',
 		);
 }
