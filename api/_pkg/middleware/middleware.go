c38c7cd0524dba86d5d4ac60d6a9862e7f14c077
Ainsley Clark <info@ainsleyclark.com>
2023-03-11 11:00:01 +0000

    style: Deleting middleware package file

--- api/_pkg/middleware/middleware.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2023 ainsley.dev LTD. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package middleware
