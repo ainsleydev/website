4bfb122c05853fb8622ff03df2382a74cf6c898e
Ainsley Clark <info@ainsleyclark.com>
2023-03-15 19:29:53 +0000

    feat: Touch/non touch for hover

--- api/_pkg/logger/logger_test.go
+++ api/_pkg/logger/logger_test.go
@@ -7,10 +7,10 @@ package logger
 import (
 	"bytes"
 	"fmt"
+	"github.com/ainsleyclark/errors"
 	"testing"
 
 	"github.com/ainsleyclark/ainsley.dev/api/_pkg/environment"
-	"github.com/ainsleyclark/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
@@ -97,12 +97,6 @@ func TestLogger(t *testing.T) {
 			},
 			"with-fields",
 		},
-		"With Error": {
-			func() {
-				WithError(&errors.Error{Code: "code", Message: "message", Operation: "op", Err: fmt.Errorf("err")}).Error()
-			},
-			"level=error",
-		},
 	}
 
 	for name, test := range tt {
@@ -117,6 +111,17 @@ func TestLogger(t *testing.T) {
 			assert.Contains(t, buf.String(), test.want)
 		})
 	}
+}
+
+func TestWithError(t *testing.T) {
+	defer func() {
+		DefaultLogger = logrus.New()
+	}()
+	buf := &bytes.Buffer{}
+	DefaultLogger.SetLevel(logrus.TraceLevel)
+	DefaultLogger.SetOutput(buf)
+	WithError(&errors.Error{Code: "code", Message: "message", Operation: "op", Err: fmt.Errorf("err")}).Error()
+	assert.Contains(t, buf.String(), "level=error")
 }
 
 func TestLogger_Fatal(t *testing.T) {
