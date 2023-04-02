409f9a25145b8fbd11d493445070d897adf221cd
Ainsley Clark <info@ainsleyclark.com>
2023-04-02 16:14:49 +0100

    feat: Home video

--- assets/js/components/video.ts
+++ assets/js/components/video.ts
@@ -14,7 +14,28 @@
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play_event|play_event}
  */
 export const video = (): void => {
+	/**
+	 * Add playing class to video when it's playing.
+	 */
 	document.querySelectorAll('video').forEach((vid) => {
 		vid.addEventListener('play', () => vid.classList.add('video-playing'));
+		if (vid.hasAttribute('data-plausible') && window.plausible) {
+			console.log('in');
+			window.plausible(vid.getAttribute('data-plausible'));
+		}
+	});
+	/**
+	 * Button click handler.
+	 */
+	document.querySelectorAll('.video-container').forEach((container) => {
+		const button = container.querySelector('.video-button'),
+			vid = container.querySelector('video') as HTMLVideoElement;
+		if (!button || !vid) {
+			return;
+		}
+		button.addEventListener('click', () => {
+			console.log('vid');
+			vid.play();
+		});
 	});
 };
