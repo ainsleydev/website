0c9454fc2633dd3ba52427c5709f22924dae3822
Ainsley Clark <hello@ainsley.dev>
2023-04-18 15:45:37 +0100

    style: Formatting video

--- assets/js/components/video.ts
+++ assets/js/components/video.ts
@@ -73,7 +73,7 @@ export const video = (): void => {
 			} else if (video.msRequestFullscreen) { /* IE11 */
 				video.msRequestFullscreen();
 			}
-			video.play();
+			playVideo(video);
 		});
 	});
 
