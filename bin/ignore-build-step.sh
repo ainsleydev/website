3f11020a25f8dfce0cd3be0ba9118df1a082bd17
Ainsley Clark <hello@ainsley.dev>
2023-04-29 17:25:48 +0100

    feat: Adding build step ignore

---
+++ bin/ignore-build-step.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Determines if the Vercel build should run
+
+echo "Vercel Env: $VERCEL_ENV"
+echo "Vercel Commit Ref: $VERCEL_GIT_COMMIT_REF"
+
+# Don't build for other branches
+if [[ "$VERCEL_GIT_COMMIT_REF" != "main" && "$VERCEL_GIT_COMMIT_REF" != "staging"  ]]; then
+	echo "🛑 - Build cancelled"
+	exit 0;
+else
+	# Proceed with the build
+	echo "✅ - Build can proceed"
+	exit 1;
+fi;
+
