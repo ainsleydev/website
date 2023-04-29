cf888954e3c92ca37107b8b858a56b7207f498ef
Ainsley Clark <info@ainsleyclark.com>
2023-04-29 09:25:55 +0100

    refactor: Updating build script to igore other branches

--- bin/build.sh
+++ bin/build.sh
@@ -9,10 +9,20 @@
 echo "Vercel Env: $VERCEL_ENV"
 echo "Vercel Commit Ref: $VERCEL_GIT_COMMIT_REF"
 
-if [[ $VERCEL_ENV == "production" ]]; then
-	echo "Building production"
-	npm run build:prod
+# Don't build for other branches
+if [[ "$VERCEL_GIT_COMMIT_REF" != "main" && "$VERCEL_GIT_COMMIT_REF" != "staging"  ]]; then
+	echo "🛑 - Build cancelled"
+	exit 0;
 else
-	echo "Building staging"
-	npm run build:staging
-fi
+  	# Proceed with the build
+	echo "✅ - Build can proceed"
+	if [[ $VERCEL_ENV == "production" ]]; then
+    	echo "Building production"
+    	npm run build:prod
+    else
+    	echo "Building staging"
+    	npm run build:staging
+    fi
+    exit 1;
+fi;
+
