5ccf2ed3a94f9e27defb42db4388baecb77873e5
Ainsley Clark <34712954+ainsleyclark@users.noreply.github.com>
2026-02-23 09:09:09 +0000

    fix: Mobile nav anchor link & missing static assets (#288)
    * fix: Nav & home bits
    
    * fix: Config/JS fixes

--- themes/shared/assets/js/pages/home.ts
+++ themes/shared/assets/js/pages/home.ts
@@ -16,8 +16,13 @@ import { Elements } from '../util/els';
 export const homeAnimation = (): void => {
 	const hero = <HTMLElement>document.querySelector('.home-hero-wrapper'),
 		wrapper = <HTMLElement>document.querySelector('.home-three-d'),
-		svg = <HTMLElement>document.querySelector('.home-three-d svg'),
-		height = hero.getBoundingClientRect().height,
+		svg = <HTMLElement>document.querySelector('.home-three-d svg')
+
+	if (!hero || !svg || !wrapper) {
+		return
+	}
+
+	const height = hero.getBoundingClientRect().height,
 		offset = 500;
 
 	const scrollTimeline = anime
